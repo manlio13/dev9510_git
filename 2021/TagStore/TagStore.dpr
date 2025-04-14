7c10db1d4491344d96cd46ae9ac1e5de6fa21032
manlio13 <manlio.laschena@gmail.com>
2025-04-14 18:34:50 +0200

    ecom 4/25

---
+++ 2021/TagStore/TagStore.dpr
@@ -0,0 +1,29 @@
+program TagStore;
+
+uses
+  {$IFDEF EurekaLog}
+  EMemLeaks,
+  EResLeaks,
+  EDebugExports,
+  EDebugJCL,
+  EFixSafeCallException,
+  EMapWin32,
+  EAppVCL,
+  EDialogWinAPIMSClassic,
+  EDialogWinAPIEurekaLogDetailed,
+  EDialogWinAPIStepsToReproduce,
+  ExceptionLog7,
+  {$ENDIF EurekaLog}
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'TagStore';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
